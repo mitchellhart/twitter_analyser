8b982c7be2cdc361b2072b78067cac0a0e86c361
AnnaErshova <anna.ershova@gmail.com>
2015-07-19 11:34:50 -0400

    improves handle validation mechanics, still needs error message

--- twitter-narcissist-app/app/controllers/home_controller.rb
+++ twitter-narcissist-app/app/controllers/home_controller.rb
@@ -1,37 +1,32 @@
 class HomeController < ApplicationController
 
-  class HandleError < StandardError
-  end
-
   def index
-
   end
 
   def new
     @handle = User.new
     @handle.name = params[:handle]
-    if @handle.name = ""
-      flash[:notice] = "This user is not activated."
-      redirect_to "/"
-    else 
+    handle_valid?
+  end
+
+  def handle_valid?
+    handle = @handle.name
+    if /^[A-Za-z0-9_]{1,15}$|^@[A-Za-z0-9_]{1,15}$/.match(handle) # returns nil or a match
       @handle.save
       @handle.run
       redirect_to "/show/#{@handle.id}"
+    else 
+      redirect_to root_path
     end
   end
 
   def show
-  
     @handle = User.find(params[:id])
     @celeb = @handle.find_closest_celeb
   end
 
-  def topten
-    
+  def topten   
     @topten = User.select(:name, :score_f).order(score_f: :desc).limit(10)
-     
-
   end
-
 
 end
